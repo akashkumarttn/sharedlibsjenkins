9e77b0bcf4fb946d0b591993f3862ccff7125436
Akash Kumar <90306521+akashkumarttn@users.noreply.github.com>
2022-02-17 09:44:02 +0530

    Update migration.groovy

--- vars/migration.groovy
+++ vars/migration.groovy
@@ -2,7 +2,7 @@ def update(year,month,level,account){
   sh "python3 scripts/cur_scripts/tag-tables-update-new.py ${year} ${month} ${level} '${accounts}'"
 }
 
-def pull(class){
+def pull(){
   sh 'git config --global http.sslverify "false"'
  	checkout([$class: 'GitSCM', branches: [[name: 'prod-migration1']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'd153af68-8f30-4c55-bfe4-646037e020f0', url: 'git@gitlab.intelligrape.net:tothenew/mycloud-scripts.git']]]) 
 }
