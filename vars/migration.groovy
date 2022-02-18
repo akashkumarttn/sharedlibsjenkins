c2583ccb0bf5eccfd697bf0e56c115f5189d72b5
Akash Kumar <90306521+akashkumarttn@users.noreply.github.com>
2022-02-18 12:58:28 +0530

    Update migration.groovy

--- vars/migration.groovy
+++ vars/migration.groovy
@@ -4,14 +4,20 @@ def update(year,month,level,account){
 
 def pull(class_name,name,url){
   
-  checkout([$class: ${class_name}, 
-            branches: [[name: ${name}]], 
+  checkout([$class: def class_name(Map), 
+            branches: [[name: def class_name(Map)]], 
             doGenerateSubmoduleConfigurations: false, 
             extensions: [], 
             submoduleCfg: [], 
-            userRemoteConfigs: [[url: ${url}]]]) 
+            userRemoteConfigs: [[url: def class_name(Map)]]]) 
 }
 
 def clean(){
   cleanWs()
+}
+
+def class_name(Map){
+  $class: GitSCM
+  name: main
+  url: 'https://github.com/akashkumarttn/sharedlibsjenkins.git'
 }
