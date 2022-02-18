77d0b87f44bf9ab0c609327d74ee725e34837f65
Akash Kumar <90306521+akashkumarttn@users.noreply.github.com>
2022-02-18 11:28:08 +0530

    Update migration.groovy

--- vars/migration.groovy
+++ vars/migration.groovy
@@ -2,9 +2,15 @@ def update(year,month,level,account){
   sh "python3 scripts/cur_scripts/tag-tables-update-new.py ${year} ${month} ${level} '${accounts}'"
 }
 
-def pull(){
-  sh 'git config --global http.sslverify "false"'
- 	checkout([$class: 'GitSCM', branches: [[name: 'prod-migration1']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'd153af68-8f30-4c55-bfe4-646037e020f0', url: 'git@gitlab.intelligrape.net:tothenew/mycloud-scripts.git']]]) 
+def pull(Map stageParams){
+  #sh 'git config --global http.sslverify "false"'
+ 	checkout([$class: 'GitSCM', 
+            branches: [[name: stageParams.branch]], 
+            doGenerateSubmoduleConfigurations: false, 
+            extensions: [], 
+            submoduleCfg: [], 
+            #userRemoteConfigs: [[credentialsId: 'd153af68-8f30-4c55-bfe4-646037e020f0', 
+            url: stageParams.url]]]) 
 }
 
 def clean(){
