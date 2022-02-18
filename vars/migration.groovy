ca1ee20284bc3ecfd2402153c7a92eb0397a56a1
Akash Kumar <90306521+akashkumarttn@users.noreply.github.com>
2022-02-18 12:49:56 +0530

    Update migration.groovy

--- vars/migration.groovy
+++ vars/migration.groovy
@@ -2,14 +2,14 @@ def update(year,month,level,account){
   sh "python3 scripts/cur_scripts/tag-tables-update-new.py ${year} ${month} ${level} '${accounts}'"
 }
 
-def pull(Map stageParams){
+def pull(class_name,name,url){
   
- 	checkout([$class: 'GitSCM', 
-            branches: [[name: stageParams.branch]], 
+  checkout([$class: ${class_name}, 
+            branches: [[name: ${name}]], 
             doGenerateSubmoduleConfigurations: false, 
             extensions: [], 
             submoduleCfg: [], 
-            userRemoteConfigs: [[url: 'https://github.com/akashkumarttn/sharedlibsjenkins.git']]]) 
+            userRemoteConfigs: [[url: ${url}]]]) 
 }
 
 def clean(){
