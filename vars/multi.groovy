0ccce87c0868227f83f26d331ff92bce56d98d22
Akash Kumar <90306521+akashkumarttn@users.noreply.github.com>
2022-03-01 13:15:46 +0530

    Update multi.groovy

--- vars/multi.groovy
+++ vars/multi.groovy
@@ -1,4 +1,9 @@
 def multi(Object... args) {
-    echo "{ ${path} ${args[0]} ${args[1]} ${args[3]} ${args[4]}}"
+    no.of_args = length(args)
+    string = "${path} ${args[0]} ${args[1]} ${args[3]} ${args[4]"
+    for value in range[0,no.of_args]:
+        string += agrs[value]
+    sh "string"
+    //echo "{ ${path} ${args[0]} ${args[1]} ${args[3]} ${args[4]}}"
    // sh "python3 scripts/cur_scripts/tag-tables-update-new.py ${args[0]} ${month} ${level} '${accounts}'"
 }
