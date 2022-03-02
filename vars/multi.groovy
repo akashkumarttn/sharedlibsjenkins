24633e39e03b4d0edcbfc0d84417937e1ef7cdbd
Akash Kumar <90306521+akashkumarttn@users.noreply.github.com>
2022-03-02 11:21:58 +0530

    Update multi.groovy

--- vars/multi.groovy
+++ vars/multi.groovy
@@ -1,4 +1,10 @@
 def multi(Object... args) {
+        class Example {
+        static void main(String[] args) {
+          String a = "Hello";
+          println(a.length());
+       } 
+    }
     no.of_args = length(args)
     string = "${path} ${args[0]} ${args[1]} ${args[3]} ${args[4] n"
     for value in range[0,no.of_args]:
